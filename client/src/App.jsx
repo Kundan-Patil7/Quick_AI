b8f381adb052bef568202fb2e60bdfdc075d9461
Kundan-Patil7 <kpatil.pale@gmail.com>
2025-09-03 16:58:01 +0530

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,11 @@ import Dashboard from './pages/Dashboard'
 import Community from './pages/community'
 
 import BlogTitles from './pages/BlogTitles'
-import GenerateImage from './pages/GenrateImages'
+import GenerateImages from './pages/GenerateImages'
 import RemoveBackground from './pages/RemoveBackground'
 import ReviewResume from './pages/ReviewResume'
 import RemoveObject from './pages/RemoveObject'
-import WriteArticle from './pages/WriteArtical'
+import WriteArticle from './pages/WriteArticle'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
@@ -33,7 +33,7 @@ const App = () => {
           <Route index element={<Dashboard />} />
           <Route path='write-article' element={<WriteArticle />} />
           <Route path='blog-titles' element={<BlogTitles />} />
-          <Route path='generate-images' element={<GenerateImage />} />
+          <Route path='generate-images' element={<GenerateImages />} />
           <Route path='remove-background' element={<RemoveBackground />} />
           <Route path='remove-object' element={<RemoveObject />} />
           <Route path='review-resume' element={<ReviewResume />} />
